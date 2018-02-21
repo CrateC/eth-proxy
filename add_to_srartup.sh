a1bd7c77dc9e229c1088f13ba990924389c61e19
CrateC <artem.boyko@gmail.com>
2018-02-22 00:00:34 +0200

    Create add_to_srartup.sh

---
+++ add_to_srartup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Copy autorun script so /etc/init.d/
+sudo cp eth-proxy/local.autostart /etc/init.d/
+
+# Make executable
+sudo chmod +x /etc/init.d/local.autostart
+
+# Configure to autostart at startup
+sudo update-rc.d local.autostart defaults 80
