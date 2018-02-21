289bf77af1c0a3720f70d0bfefe98f2dd5d3d5df
CrateC <artem.boyko@gmail.com>
2018-02-22 00:12:45 +0200

    Update add_to_srartup.sh

--- add_to_srartup.sh
+++ add_to_srartup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Copy autorun script so /etc/init.d/
+echo Copy autorun script so /etc/init.d/
 sudo cp local.autostart /etc/init.d/
 
 # Make executable
+echo Make executabl
 sudo chmod +x /etc/init.d/local.autostart
 
 # Configure to autostart at startup
+echo Configure to autostart at startup
 sudo update-rc.d local.autostart defaults 80
