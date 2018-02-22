0c1a323a2aeb8d1a722afed58cd31fb40c7e4f9a
CrateC <artem.boyko@gmail.com>
2018-02-22 02:17:05 +0200

    Update add_to_srartup.sh

--- add_to_srartup.sh
+++ add_to_srartup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Copy autorun script so /etc/init.d/
-echo ">>" ETH-PROXY SETUP :: Copy autorun script so /etc/init.d/
+echo :: ETH-PROXY SETUP :: Copy autorun script so /etc/init.d/
 sudo cp local.autostart /etc/init.d/
 
 # Make executable
-echo ">>" ETH-PROXY SETUP :: Make executabl
+echo :: ETH-PROXY SETUP :: Make executabl
 sudo chmod +x /etc/init.d/local.autostart
 
 # Configure to autostart at startup
-echo ">>" ETH-PROXY SETUP :: Configure to autostart at startup
+echo :: ETH-PROXY SETUP :: Configure to autostart at startup
 sudo update-rc.d local.autostart defaults 80
 
 sleep 1
 
-echo ">>" ETH-PROXY SETUP :: Rebooting...
-sudo reboot
+echo :: ETH-PROXY SETUP :: Rebooting...
+reboot
